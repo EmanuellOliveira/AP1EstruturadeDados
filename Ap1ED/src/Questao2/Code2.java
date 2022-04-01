7da378b4f398b3059323e17939f7f80192ac904f
Emanuell Oliveira <emanuell.oliveirajs@gmail.com>
2022-04-01 00:05:25 -0300

    Inicial

---
+++ Ap1ED/src/Questao2/Code2.java
@@ -0,0 +1,16 @@
+package Questao2;
+import java.util.Scanner;
+
+public class Code2 {
+    
+    public static void main(String[]args){
+        int Num;
+        Scanner input = new Scanner(System.in);
+        
+        Num = input.nextInt();
+
+        System.out.println("Número " + Num);
+    }
+
+
+}
