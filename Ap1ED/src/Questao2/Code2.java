f4b485a557ac0916020179b352a8ad00e8e15dba
Nell <93164015+NellOliveira@users.noreply.github.com>
2022-04-02 19:58:42 -0300

    Feito até a Questão 3

--- Ap1ED/src/Questao2/Code2.java
+++ Ap1ED/src/Questao2/Code2.java
@@ -4,16 +4,23 @@ import java.util.Scanner;
 public class Code2 {
     
     public static void main(String[]args){
-        
+
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Insira um número: " );
-        String Num = input.nextLine();
+        System.out.print("Informe o número: ");
+        int x = input.nextInt();
+        
+        System.out.printf("O inverso de " + x + " é ");
+        Inverse(x);
+    } 
 
-        System.out.println(Inverse(Num));
-    }
-    public static String Inverse(String inv){
-
-        return new StringBuilder(inv).reverse().toString();
-    }
+    public static int Inverse(int inv){
+        
+        if(inv==0) return inv;
+            else{
+                System.out.printf("%d",inv % 10);
+                inv = inv/10;
+                return Inverse(inv);
+            }
+    } 
 }
